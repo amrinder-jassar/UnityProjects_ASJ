13cadbf33ad8f50b7d9cf8f00087b53e11881c34
amrinder-jassar <amrinder.s.jassar@gmail.com>
2021-02-12 05:09:42 -0800

    Floating Mechanic added

--- EndlessRunner/Assets/Scripts/PLayerBehaviour.cs
+++ EndlessRunner/Assets/Scripts/PLayerBehaviour.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PLayerBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject capsuleObj;
+    
     void Start()
     {
         
-    }
+        //###################  HOW TO PRINT ROTATAIONS & RANDON NUMBERS ###########
+        Debug.LogFormat("Printing Euler Angles:{0}",transform.eulerAngles);// World Rotations
+        Debug.LogFormat("Printing Local Euler Angles:{0}", transform.localEulerAngles);// Local Rotations
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < 10; i++)
+        {   
+            GameObject myObj = Instantiate(capsuleObj);
+            myObj.transform.position = new Vector3(i*2f, 1f, 1f);
+            myObj.transform.localScale = new Vector3(Random.Range(-5f,5f),i*1f,1f);
+        }
+
     }
 }
