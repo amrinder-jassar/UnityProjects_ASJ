7a1a449a6a8679541e194fb409a19f40fedaf339
amrinder-jassar <amrinder.s.jassar@gmail.com>
2021-02-13 14:52:01 -0800

    Updated forward motion

--- EndlessRunner/Assets/Scripts/PlayerCubeController.cs
+++ EndlessRunner/Assets/Scripts/PlayerCubeController.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class PlayerCubeController : MonoBehaviour
 {
-    //############## SIMPLE JUMP AND RIGht LEFT MOtion ###################
-    float hInput;
-    float vInput;
+    //############## SIMPLE JUMP AND RIGht LEFT MOtion 2 VR concept start ###################
     public float moveSpeed;
     public float rotSpeed;
     public float jumpSpeed;
@@ -20,18 +18,25 @@ public class PlayerCubeController : MonoBehaviour
     
     void Update()
     {
-        hInput = Input.GetAxis("Horizontal")*moveSpeed*Time.deltaTime;
-        vInput = Input.GetAxis("Vertical")*moveSpeed*Time.deltaTime;   
-    }
-    private void FixedUpdate()
-    {
-        rb.AddForce(hInput, 0f, vInput);
+        if (Input.GetKey("up"))
+        {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.RotateAround(transform.position, Vector3.up, rotSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.RotateAround(transform.position, Vector3.up, -rotSpeed * Time.deltaTime);
+        }
         if (Input.GetKeyDown("space") && canJump == true)
         {
             rb.AddForce(Vector3.up * jumpSpeed);
             canJump = false;
         }
     }
+ 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Plane" || collision.gameObject.name == "Ramp")
