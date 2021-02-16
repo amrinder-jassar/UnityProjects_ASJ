6f9d90708089e25a30a84d120d2f38eeb987985c
amrinder-jassar <amrinder.s.jassar@gmail.com>
2021-02-15 18:47:34 -0800

    Update CylinderAmr01.cs

--- VRProject01/Assets/Scripts/CylinderAmr01.cs
+++ VRProject01/Assets/Scripts/CylinderAmr01.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class CylinderAmr01 : MonoBehaviour
 {
-    public float[] myHeight = {1.1f,2.2f,3.3f,4.4f,5.5f };
-    void Start()
+    //########## Creating and filling in ARRAYS #############
+    public GameObject[] Towers;
+    private void Start()
     {
-        
-        for(int i =0; i <4; i++)
+        Towers = GameObject.FindGameObjectsWithTag("Tower");
+        for(int i=0; i<Towers.Length; i++)
         {
-            Debug.Log(myHeight[i]);
+            Debug.Log("Tower no "+i+" has name"+Towers[i]);
+            Debug.LogFormat("Z");
         }
     }
 }
