9f097f100497526f2b9071cfd73ecdfb1c7deae6
amrinder-jassar <amrinder.s.jassar@gmail.com>
2021-02-20 20:44:15 -0800

    Random from list

--- WheckAMole/Assets/Scripts/FindRandomPosition.cs
+++ WheckAMole/Assets/Scripts/FindRandomPosition.cs
@@ -5,67 +5,28 @@ using UnityEngine;
 public class FindRandomPosition : MonoBehaviour
 {
     public List <Transform> locations;
-    public List<int> indexCheck;
+    public List<int> IndexCheck;
     public Vector3 InstantiatePosition;
     public GameObject Capsule;
+
+    
     
 
     private void Start()
-    {        
-        InitializePopPositionsList();
-    }
-
-    void Update()
-    {
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            int index = Random.Range(0, 9);
-            for (int i=0;i<9;i++)
-            {
-                if (indexCheck.Count==0)
-                {
-                    indexCheck.Add(index);
-                    Debug.Log("Count ZERO");
-                }
-                else if (index == indexCheck[i])
-                {
-                    Debug.Log("Already Exist in LIST");
-                }
-                else if (index != indexCheck[i])
-                {
-                    indexCheck.Add(index);
-                    PrintList(indexCheck);
-                }
-            }
-            
-        }
-                        
-            //InstantiatePosition = locations[index].transform.position;
-            //Instantiate(Capsule, InstantiatePosition,Quaternion.identity);
-        
-        
-    }
-    //####################################################################
-    //           METHODS DECLARED AHEAD
-    //####################################################################
-    void InitializePopPositionsList()
     {
         Transform popPositions = GameObject.Find("PopPositions").transform;
         foreach (Transform child in popPositions)
         {
             locations.Add(child);
         }
-        
     }
 
-    void PrintList(List<int>listName)
+    void Update()
     {
-        Debug.Log("PRINTING LIST");
-        for (int index = 0; index < listName.Count;index++)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log(listName[index]);
+            
         }
-    } 
-   
+        
+    }
 }
