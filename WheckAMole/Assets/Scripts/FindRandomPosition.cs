f7d7344ca7a344a83246b9c67a35b8aba25439bb
amrinder-jassar <amrinder.s.jassar@gmail.com>
2021-02-20 22:13:35 -0800

    Secondry Update on List of positions

--- WheckAMole/Assets/Scripts/FindRandomPosition.cs
+++ WheckAMole/Assets/Scripts/FindRandomPosition.cs
@@ -25,8 +25,41 @@ public class FindRandomPosition : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            
+            int no = Random.Range(0,8);
+
+            if(IndexCheck.Count >=0 && IndexCheck.Count <=8)
+            {
+                IndexCheck.Add(no);
+                for (int i = 0; i < IndexCheck.Count; i++)
+                {
+                    if (no == IndexCheck[i])
+                    {
+                        Debug.Log("This No Already Exist in LIst, DO Nothing");
+                    }
+                    else if(no != IndexCheck[i])
+                    {
+                        IndexCheck.Add(no);
+                        Debug.Log("This No is not in List and is ADDED");
+                    }
+                }
+
+                       
+            }
+            else
+            {                
+                IndexCheck.RemoveAt(0);
+            }
+
+
+            for (int y =0;y<IndexCheck.Count;y++)
+            {
+                Debug.Log(IndexCheck[y]);
+            }
+            Debug.LogFormat("LIST COUNT IS:{0}",IndexCheck.Count);
+            Vector3 SpawnPos =  locations[no].transform.position;
+            Instantiate(Capsule,SpawnPos,Quaternion.identity);
         }
         
     }
+   
 }
